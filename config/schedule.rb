734ea3f9ade9d8e64a33bad871a14b202f8a826e
Luke Karrys <lukekarrys@gmail.com>
2012-12-31 14:25:38 -0700

    set whenever output

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,10 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
+
+set :output, "/home/lkarrys/www/instagram.lukelov.es/logs/cron.log"
+
+
 every 1.minutes do
   rake "recent_instagrams"
   command "jekyll"
