24a15764a3f5a143b38847a08052bd727204a334
Luke Karrys <lukekarrys@gmail.com>
2012-12-31 12:53:16 -0700

    add last compiled date and comment out realtime api post exec

--- proxy/instagram-realtime.php
+++ proxy/instagram-realtime.php
@@ -3,7 +3,7 @@
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_GET['hub_mode'] === 'subscribe' && $_GET['hub_verify_token'] === 'jekyll_instagram') {
   echo $_GET['hub_challenge'];
 } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  exec('cd ../../jekyll-instagram/ && sh recent.sh');
+  //exec('cd ../../jekyll-instagram/ && sh recent.sh');
 }
 
 ?>
