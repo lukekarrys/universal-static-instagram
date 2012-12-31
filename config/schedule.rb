a8d0d5e9c8fd793a30e3fa03e9a4421a85b5496d
Luke Karrys <lukekarrys@gmail.com>
2012-12-31 14:21:27 -0700

    add whenever

---
+++ config/schedule.rb
@@ -0,0 +1,12 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+every 1.minutes do
+  rake "recent_instagrams"
+  command "jekyll"
+  command "sh deploy.sh"
+end
+
+# Learn more: http://github.com/javan/whenever
--- recent.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rake recent_instagrams && jekyll && sh deploy.sh
