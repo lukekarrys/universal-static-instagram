3de65910b96d34d535b7e4682719be8855c211d9
Luke Karrys <lukekarrys@gmail.com>
2013-01-01 23:53:47 -0700

    Comment out realtime endpoint

--- source/proxy/instagram-realtime.php
+++ source/proxy/instagram-realtime.php
@@ -3,7 +3,8 @@
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_GET['hub_mode'] === 'subscribe' && $_GET['hub_verify_token'] === 'jekyll_instagram') {
   echo $_GET['hub_challenge'];
 } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  exec("/bin/bash -c 'source /home/lkarrys/.profile && source /home/lkarrys/.rvm/environments/ruby-1.9.3-p362@jekyll-instagram && cd /home/lkarrys/www/instagram.lukelov.es/jekyll-instagram/ && rake all' > /home/lkarrys/www/instagram.lukelov.es/logs/push.log 2>&1");
+  #exec("/bin/bash -c 'source /home/lkarrys/.profile && source /home/lkarrys/.rvm/environments/ruby-1.9.3-p362@jekyll-instagram && cd /home/lkarrys/www/instagram.lukelov.es/jekyll-instagram/ && rake all' > /home/lkarrys/www/instagram.lukelov.es/logs/push.log 2>&1", &$output);
+  #echo $output;
 }
 
 ?>
