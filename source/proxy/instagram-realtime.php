77c63f0b2fd4186a5d7023c751770581985795d7
Luke Karrys <lukekarrys@gmail.com>
2013-01-06 14:26:01 -0700

    test new php realtime endpoint

--- source/proxy/instagram-realtime.php
+++ source/proxy/instagram-realtime.php
@@ -1,10 +1,11 @@
 <?php
-
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_GET['hub_mode'] === 'subscribe' && $_GET['hub_verify_token'] === 'jekyll_instagram') {
+/*if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_GET['hub_mode'] === 'subscribe' && $_GET['hub_verify_token'] === 'jekyll_instagram') {
   echo $_GET['hub_challenge'];
-} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  #exec("/bin/bash -c 'source /home/lkarrys/.profile && source /home/lkarrys/.rvm/environments/ruby-1.9.3-p362@jekyll-instagram && cd /home/lkarrys/www/instagram.lukelov.es/jekyll-instagram/ && rake all' > /home/lkarrys/www/instagram.lukelov.es/logs/push.log 2>&1", &$output);
-  #echo $output;
-}
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {*/
+  echo "hey";
+  $output = `/bin/bash -c 'source /home/lkarrys/.profile && source /home/lkarrys/.rvm/environments/ruby-1.9.3-p362@jekyll-instagram && cd /home/lkarrys/www/instagram.lukelov.es/jekyll-instagram/ && /home/lkarrys/.rvm/bin/rake-ruby-1.9.3-p362@jekyll-instagram gen_deploy'`;
+  print_r($output);
+  echo "done";
+//}
 
 ?>
