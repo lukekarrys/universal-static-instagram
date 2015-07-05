c76765476b6f8cda6f5c255435ac9d9184bcea39
Luke Karrys <lukekarrys@gmail.com>
2015-07-05 00:49:25 -0700

    get id from photo

--- src/components/pages/Photo.js
+++ src/components/pages/Photo.js
@@ -30,7 +30,7 @@ const Photo = React.createClass({
 
   render () {
     const {photo} = this.props;
-    const {id} = this.props.photo;
+    const {id} = photo;
     return (
       id ? <div>
         <h1><PhotoTitle {...photo} /></h1>
