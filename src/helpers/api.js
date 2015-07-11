b66d2d181b84394e8b1e17099346dacce8eb7fe1
Luke Karrys <lukekarrys@gmail.com>
2015-07-10 18:07:52 -0700

    remove timeout from testing

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -5,11 +5,11 @@ import tryit from 'tryit';
 import slash from './slash';
 
 const api = (path, cb) => {
-  setTimeout(() => xhr(`/json${slash(path)}.json`, (xhrErr, resp, body) => {
+  xhr(`/json${slash(path)}.json`, (xhrErr, resp, body) => {
     if (xhrErr) return cb(xhrErr);
     let json;
     tryit(() => json = JSON.parse(body), (err) => cb(err, json));
-  }), 1000);
+  });
 };
 
 const apiActions = (ctx, path) => {
