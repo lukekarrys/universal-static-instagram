eb32f2fc511ae1cf939b69a62b5474d47a127033
Luke Karrys <lukekarrys@gmail.com>
2015-07-11 23:34:08 -0700

    make copy media part of webpack build

---
+++ server/copyMedia.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import cpr from 'cpr';
+import path from 'path';
+import noop from 'lodash/utility/noop';
+import CacheDir from './cacheDir';
+
+const copyMedia = (cb) => cpr(
+  new CacheDir().media(),
+  path.resolve(__dirname, '..', 'public', 'media'),
+  {
+    deleteFirst: false,
+    overwrite: false
+  },
+  typeof cb === 'function' ? cb : noop
+);
+
+export default copyMedia;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
+/* eslint no-var:0 */
+
 'use strict';
 
 require('babel/register');
 
-module.exports = require('hjs-webpack')({
+var OnBuildPlugin = require('on-build-webpack');
+var copyMedia = require('./server/copyMedia');
+
+var config = require('hjs-webpack')({
   in: 'src/main.js',
   out: 'public',
   clearBeforeBuild: true,
   output: {hash: true},
   html: require('./server/buildStatic')
 });
+
+config.plugins.push(new OnBuildPlugin(copyMedia));
+
+module.exports = config;
