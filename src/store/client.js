336463c92b5595f4d3d100a81b1141c8642ad05b
Luke Karrys <lukekarrys@gmail.com>
2016-02-10 10:29:12 -0700

    no need for persistState really

--- src/store/client.js
+++ src/store/client.js
@@ -7,15 +7,9 @@ const middleware = [];
 
 // Only require devtools/logger based on flag so they dont get bundled
 if (__DEVTOOLS__) {
-  const {persistState} = require('redux-devtools');
   const DevTools = require('../containers/DevTools');
   storeEnhancers.push(
-    DevTools.instrument(),
-    persistState(() => {
-      // By default we try to read the key from ?debug_session=<key> in the address bar
-      const matches = window.location.href.match(/[?&]debug_session=([^&]+)\b/);
-      return (matches && matches.length > 0) ? matches[1] : null;
-    })
+    DevTools.instrument()
   );
 }
 
