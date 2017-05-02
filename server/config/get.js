317b71d9991a2b9c305ada81d28a50e218b99d38
Luke Karrys <lukekarrys@gmail.com>
2017-05-02 11:50:34 -0700

    attempt config parsing

--- server/config/get.js
+++ server/config/get.js
@@ -2,9 +2,10 @@
 
 import path from 'path';
 import fs from 'fs';
+import {attempt, isError} from 'lodash';
 
 const {USI_CONFIG} = process.env;
 const configPath = path.resolve(__dirname, '..', '..', USI_CONFIG || 'config.json');
-const config = JSON.parse(fs.readFileSync(configPath));
+const config = attempt(() => JSON.parse(fs.readFileSync(configPath)));
 
-export default () => config;
+export default () => isError(config) ? {} : config;
