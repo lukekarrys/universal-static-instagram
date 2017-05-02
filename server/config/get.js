32b9e58e1599eda95ced5a8818b454f71d4d9208
Luke Karrys <lukekarrys@gmail.com>
2017-05-02 12:09:44 -0700

    test config

--- server/config/get.js
+++ server/config/get.js
@@ -8,4 +8,9 @@ const {USI_CONFIG} = process.env;
 const configPath = path.resolve(__dirname, '..', '..', USI_CONFIG || 'config.json');
 const config = attempt(() => JSON.parse(fs.readFileSync(configPath)));
 
-export default () => isError(config) ? {} : config;
+export default () => {
+  if (isError(config)) {
+    throw new Error(`Config could not be found or malformed from ${configPath}`);
+  }
+  return config;
+};
