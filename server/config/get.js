08d6a53444436d8f0fdc0200026773d38bfe8ff5
Luke Karrys <lukekarrys@gmail.com>
2017-05-02 12:16:51 -0700

    cant throw and error there oops

--- server/config/get.js
+++ server/config/get.js
@@ -8,9 +8,4 @@ const {USI_CONFIG} = process.env;
 const configPath = path.resolve(__dirname, '..', '..', USI_CONFIG || 'config.json');
 const config = attempt(() => JSON.parse(fs.readFileSync(configPath)));
 
-export default () => {
-  if (isError(config)) {
-    throw new Error(`Config could not be found or malformed from ${configPath}`);
-  }
-  return config;
-};
+export default () => (isError(config)) ? {} : config;
